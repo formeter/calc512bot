8f18b4dfc7780d33dd5939637551ec0a0d5c6e24
Nikolay Ab <ab.nikolay@yandex.ru>
2017-12-01 15:45:10 +0300

    non-interactive adding bot with telegraf

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ const Telegraf = require('telegraf')
 const bot = new Telegraf(BOT_TOKEN)
 bot.start((ctx) => {
   console.log('started:', ctx.from.id)
-  return ctx.reply('Welcome!')
+  return ctx.reply('Welcome! Use /help')
 })
-bot.command('help', (ctx) => ctx.reply('Try send a sticker!'))
+
+var counter = 0;
+
+bot.command('help', (ctx) => ctx.reply('Reset to 0 /reset \nAdd 1 /add \nShow current value /get'))
+bot.command('get', (ctx) => ctx.reply('Value = ' + counter))
+bot.command('reset', (ctx) => { 
+    counter = 0;
+    ctx.reply('Value = ' + counter)
+})
+bot.command('add', (ctx) => { 
+    counter++;
+    ctx.reply('Value = ' + counter)
+})
+
+
 bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
 bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
