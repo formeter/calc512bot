d42399dcec5c0ee1f7ff7234267277888a491d32
Nikolay Ab <ab.nikolay@yandex.ru>
2017-12-01 11:27:48 +0300

    Codeday bot from https://blog.srnd.org/intro-to-node-js-making-a-telegram-bot-964b8cfe1129

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ var TelegramBot = require('node-telegram-bot-api'),
     // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
     telegram = new TelegramBot("490842395:AAF51fw-gy4b2eEh7DOMSkeH0fiyCUQM-qA", { polling: true });
 
+var Clear = require('codeday-clear'),
+    // Our sample app token and secret
+    clear = new Clear("1YZiGaj3baaLU8IKVsASRIWaNF2oJNg0", "1COMnWyGnGBsNqkhaZ6WMBWB9UWZw6QZ");
+
+// moment is not a class, just a simple function
+var moment = require('moment');
+
 telegram.on("text", (message) => {
-  telegram.sendMessage(message.chat.id, "Hello world");
+  if(message.text.toLowerCase().indexOf("/codeday") === 0){
+    clear.getEventById("oo4QIuKQQTYA", (codedayEvent) => {
+      var endsAt = moment(codedayEvent.ends_at * 1000);
+      telegram.sendMessage(message.chat.id, "CodeDay ends " + endsAt.fromNow() + "!");
+    });
+  }
 });
+
+
